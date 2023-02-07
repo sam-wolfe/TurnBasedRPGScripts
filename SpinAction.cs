1226084335a3968c58bbd03b81d6fe2e9e4697d7
sam-wolfe <samwolfe37@gmail.com>
2023-02-07 08:00:37 -0800

    Spin challenge

--- SpinAction.cs
+++ SpinAction.cs
@@ -5,15 +5,34 @@ using UnityEngine;
 public class SpinAction : MonoBehaviour {
 
     private bool startSpinning;
+    [SerializeField] private float spinAmount = 360;
+    private Vector3 originalRotation;
+    private float totalSpin;
 
     void Update() {
         if (startSpinning) {
-            float addSpinAmount = 360f * Time.deltaTime;
-            transform.eulerAngles += new Vector3(0, addSpinAmount,0);
+            totalSpin += ProcessSpin();
+            if (totalSpin >= spinAmount) {
+                // we are done spinning
+                EndSpin();
+            }
         }
+    }
+    
+    private float ProcessSpin() {
+        float addSpinAmount = 360f * Time.deltaTime;
+        transform.eulerAngles += new Vector3(0, addSpinAmount,0);
+        return addSpinAmount;
+    }
+    
+    private void EndSpin() {
+        startSpinning = false;
+        transform.eulerAngles = originalRotation;
+        totalSpin = 0;
     }
 
     public void Spin() {
         startSpinning = true;
+        originalRotation = transform.eulerAngles;
     }
 }
