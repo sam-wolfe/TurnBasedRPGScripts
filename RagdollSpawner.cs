9de262a1a70233b127e614cc23c61491a94648c7
sam-wolfe <samwolfe37@gmail.com>
2023-02-17 08:55:14 -0800

    Added death, added ragdoll spawn on death

--- RagdollSpawner.cs
+++ RagdollSpawner.cs
@@ -11,6 +11,18 @@ public class RagdollSpawner : MonoBehaviour {
     private void Awake() {
         _healthSystem = GetComponent<HealthSystem>();
         
+        _healthSystem.OnDeath += SpawnRagdoll;
+        
+    }
+    
+    private void SpawnRagdoll(Unit unit) {
+        Transform ragdollTransform = Instantiate(ragdollPrefab, transform.position, transform.rotation);
+        // Apply a small random force to make it look more ragdoll-y
+        
+        float randomForce = UnityEngine.Random.Range(100f, 500f);
+        var head = ragdollTransform.GetComponentInChildren<Rigidbody>();
+        var hrb = head.GetComponent<Rigidbody>();
+        hrb.AddForce(new Vector3(0, 1, 0) * randomForce);
     }
 
 }
