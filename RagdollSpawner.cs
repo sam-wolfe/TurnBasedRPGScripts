d52f6f53d7e183e963964a63129b0fb751980828
sam-wolfe <samwolfe37@gmail.com>
2023-02-17 16:56:26 -0800

    Position ragdolls in original position on death

--- RagdollSpawner.cs
+++ RagdollSpawner.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class RagdollSpawner : MonoBehaviour {
 
     [SerializeField] private Transform ragdollPrefab;
+    [SerializeField] private Transform originalRoot;
     private HealthSystem _healthSystem;
 
     private void Awake() {
@@ -17,12 +18,16 @@ public class RagdollSpawner : MonoBehaviour {
     
     private void SpawnRagdoll(Unit unit) {
         Transform ragdollTransform = Instantiate(ragdollPrefab, transform.position, transform.rotation);
-        // Apply a small random force to make it look more ragdoll-y
+
+        // Copy the position and rotation of the unit to the ragdoll using Setup 
+        ragdollTransform.GetComponent<UnitRagdoll>().Setup(originalRoot);
         
         float randomForce = UnityEngine.Random.Range(100f, 500f);
         var head = ragdollTransform.GetComponentInChildren<Rigidbody>();
         var hrb = head.GetComponent<Rigidbody>();
         hrb.AddForce(new Vector3(0, 1, 0) * randomForce);
+        
+        hrb.AddTorque(new Vector3(0, 1, 0) * randomForce);
     }
 
 }
---
+++ UnitRagdoll.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitRagdoll : MonoBehaviour {
+
+    [SerializeField] private Transform _ragdollRoot;
+
+    public void Setup(Transform originalRoot) {
+        MatchAllChildTransforms(originalRoot, _ragdollRoot);
+    }
+
+    private void MatchAllChildTransforms(Transform root, Transform clone) {
+        foreach (Transform child in root) {
+            Transform cloneChild = clone.Find(child.name);
+            if (cloneChild != null) {
+                cloneChild.position = child.position;
+                cloneChild.rotation = child.rotation;
+                MatchAllChildTransforms(child, cloneChild);
+            }
+        }
+    }
+    
+}
