165660c125122cf5fc2acc4733367de3767b2a2c
sam-wolfe <samwolfe37@gmail.com>
2023-02-20 06:42:17 -0800

    EnemyAI states

--- EnemyAI.cs
+++ EnemyAI.cs
@@ -1,20 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour {
 
+    private enum State {
+        WaitingForTurn,
+        TakingTurn,
+        Busy
+    }
+    private State _state;
+    
     public float timer;
+
+    private void Awake() {
+        _state = State.WaitingForTurn;
+    }
+
+    private void OnEnable() {
+        TurnSystem.instance.OnTurnChanged += HandleTurnChange;
+    }
+    
+    private void OnDisable() {
+        TurnSystem.instance.OnTurnChanged -= HandleTurnChange;
+    }
 
     void Update() {
 
-        if (!TurnSystem.instance.IsPlayerTurn()) {
-            timer -= Time.deltaTime;
-            if (timer <= 0) {
-                timer = 3;
-                TurnSystem.instance.NextTurn();
-            }
+        if (TurnSystem.instance.IsPlayerTurn()) {
+            
+            return;
+        }
+        
+        switch (_state) {
+            case State.WaitingForTurn:
+                break;
+            case State.TakingTurn:
+                timer -= Time.deltaTime;
+                if (timer <= 0) {
+                    _state = State.WaitingForTurn;
+                    TurnSystem.instance.NextTurn();
+                }
+                break;
+            case State.Busy:
+                
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
         }
 
+
+    }
+
+    private void HandleTurnChange() {
+        if (!TurnSystem.instance.IsPlayerTurn()) {
+            _state = State.TakingTurn;
+            timer = 3;
+        }
     }
 }
