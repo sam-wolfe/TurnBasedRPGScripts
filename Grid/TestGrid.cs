b7dec863d9d4cf31436b6be387d598213480c136
sam-wolfe <samwolfe37@gmail.com>
2023-02-24 21:34:47 -0800

    .

--- Grid/TestGrid.cs
+++ Grid/TestGrid.cs
@@ -10,12 +10,6 @@ public class TestGrid : MonoBehaviour {
     [SerializeField] private Unit unit;
     [SerializeField] private GridSystemVisual _gridSystemVisual;
     
-    void Start() {
-        // _gridSystem = new GridSystem(10, 10, 2f);
-        // _gridSystem.CreateDebugObjects(_gridDebugObjectPrefab);
-
-        // Debug.Log(new GridPosition(5, 7));
-    }
 
     private void Update() {
         // Debug.Log(_gridSystem.GetGridPosition(MouseWorld.GetPosition()));
